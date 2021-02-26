bfe8697cc874e27f73f639579491aa0ca16bd1fb
Ryan Brunkhorst <16407142+ryanbrunkhorst@users.noreply.github.com>
2021-02-26 14:24:03 -0800

    Add files via upload

--- Find Certificate_v3.ps1
+++ Find Certificate_v3.ps1
@@ -1,7 +1,7 @@
 ﻿$CurrentDate = Get-Date #get date
 $CurrentDate = $CurrentDate.ToString('MM-dd-yyyy-hh-mm-ss') #convert date to this format
-New-Item -Path "$PSScriptRoot\results" -Name $CurrentDate -ItemType "directory" #create a folder with date and time script is run
-Copy-Item "$PSScriptRoot\computers.txt" -Destination "$PSScriptRoot"\results\"$CurrentDate" #copies computers.txt to results date folder for recording purposes
+New-Item -Path "$PSScriptRoot\results\" -Name $CurrentDate -ItemType "directory" #create a folder with date and time script is run
+Copy-Item "$PSScriptRoot\computers.txt" -Destination $PSScriptRoot"\results\"$CurrentDate #copies computers.txt to results date folder for recording purposes
 
 $computers = get-content "$PSScriptRoot\computers.txt" #imports the list of computers
 
@@ -14,14 +14,14 @@ echo ("Checking " + $computer)
 $results = Invoke-Command -ComputerName $computer -ScriptBlock {Get-ChildItem -Path Cert:\LocalMachine\Root |`
 Where-Object {$_.Thumbprint -eq "D2D18014060C598B081F4EDE670A08899D409519"}}
 
-if (!$results) {echo $computer | out-file $PSScriptRoot"\results\"$CurrentDate"\Z_ComputersMissingCert_"$CurrentDate.txt -Append} #if the cert is missing or WinRM fails
-    else {echo $computer | out-file $PSScriptRoot"\results\"$CurrentDate"\Z_ComputersWithCert_"$CurrentDate.txt -Append} #if the cert is found add to this txt file
+if (!$results) {echo $computer | out-file $PSScriptRoot"\results\"$CurrentDate"\ComputersMissingCertorWinRMError_"$CurrentDate.txt -Append} #if the cert is missing or WinRM fails
+    else {echo $computer | out-file $PSScriptRoot"\results\"$CurrentDate"\ComputersWithCert_"$CurrentDate.txt -Append} #if the cert is found add to this txt file
 
 }
 
 else{ #if test-connection fails, add computer name to offline text file
 echo $computer" offline"
-echo $computer | out-file $PSScriptRoot"\results\"$CurrentDate"\Z_Offline_"$CurrentDate.txt -Append
+echo $computer | out-file $PSScriptRoot"\results\"$CurrentDate"\Offline_"$CurrentDate.txt -Append
 }
 
 }
